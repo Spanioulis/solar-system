2a641ae8ac7452d1dfeb1757e62ca3556bc28b33
Sergio Paniagua Sánchez <97700906+Spanioulis@users.noreply.github.com>
2023-06-11 21:18:59 +0200

    style: complete variables and mixins

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,24 @@ import styles from '../Home.module.scss';
 
 export default function Home() {
    return (
-      <main className={styles.main}>
-         <h1>Solar System</h1>
-         <a>Probando cursor pointer</a>
-         <p>Texto normal...</p>
-      </main>
+      <>
+         <header className={styles.header}>
+            <nav className={styles.navbar}>
+               <h1>Solar System</h1>
+               <a href="https://www.nasa.gov/" target="_blank" rel="noreferrer noopener">
+                  NASA
+               </a>
+               <a
+                  href="https://spanioulis.dev/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  className={styles['second-anchor']}
+               >
+                  @Spanioulis
+               </a>
+            </nav>
+         </header>
+         <main className={styles.main}></main>
+      </>
    );
 }

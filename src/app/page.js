cfb5133ff8932ef4d92a09aa372326c58523845a
Sergio Paniagua Sánchez <97700906+Spanioulis@users.noreply.github.com>
2023-06-12 21:54:41 +0200

    add background Image

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
+import Image from 'next/image';
 import styles from '../Home.module.scss';
 
 export default function Home() {
    return (
       <>
+         <div style={{ zIndex: -1, position: 'fixed', width: '100vw', height: '100vh', opacity: 0.2 }}>
+            <Image src="/assets/stars.png" alt="Sticky star" fill style={{ objectFit: 'cover' }} />
+         </div>
          <header className={styles.header}>
             <nav className={styles.navbar}>
                <h1>Solar System</h1>

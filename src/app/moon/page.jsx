eaf08b90aead55588c2347bc0d74a1a318af068b
Sergio Paniagua Sánchez <97700906+Spanioulis@users.noreply.github.com>
2023-06-25 20:00:38 +0200

    add planet_card

--- src/app/moon/page.jsx
+++ src/app/moon/page.jsx
@@ -1,6 +1,3 @@
-// 'use client';
-
-// import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from '../../styles/pages/Moon.module.scss';
@@ -17,6 +14,7 @@ async function getData() {
 
 export default async function Moon() {
    const data = await getData();
+   const moon = data.bodies[0];
 
    return (
       <>
@@ -27,10 +25,8 @@ export default async function Moon() {
          </div>
          <header className={styles.header}>
             <nav className={styles.navbar}>
-               <Image src="/assets/moon.png" alt="Moon" width={100} height={100} />
-               <Image src="/assets/blue_moon.png" alt="Moon" width={100} height={100} />
-               <Image src="/assets/red_moon.png" alt="Moon" width={100} height={100} />
-               <Image src="/assets/moon.png" alt="Moon" width={100} height={100} />
+               <Image src="/assets/moon_2.png" alt="Moon" width={115} height={115} />
+
                <Link href="/">
                   {' '}
                   <Image src="/assets/atronaut_01.png" width={50} height={50} alt="Astronaut image" />
@@ -40,7 +36,61 @@ export default async function Moon() {
          <main className={styles.main}>
             {data ? (
                <>
-                  <h3>{data.bodies[0].name}</h3>
+                  <h2>{moon?.englishName}</h2>
+                  <div className={styles.planet_card} key={moon?.id}>
+                     <div className="block-left">
+                        <p>
+                           <strong>Is Planet:</strong> {moon?.isPlanet ? 'Yes' : 'No'}
+                        </p>
+                        <p>
+                           <strong>Semimajor Axis:</strong> {moon?.semimajorAxis} km
+                        </p>
+                        <p>
+                           <strong>Perihelion:</strong> {moon?.perihelion} km
+                        </p>
+                        <p>
+                           <strong>Aphelion:</strong> {moon?.aphelion} km
+                        </p>
+                        <p>
+                           <strong>Eccentricity:</strong> {moon?.eccentricity}
+                        </p>
+                        <p>
+                           <strong>Inclination:</strong> {moon?.inclination}&deg;
+                        </p>
+                        <p>
+                           <strong>Mass:</strong> {moon?.mass?.massValue} x 10^{moon?.mass?.massExponent} kg
+                        </p>
+                        <p>
+                           <strong>Volume:</strong> {moon?.vol?.volValue} x 10^{moon?.vol?.volExponent} km^3
+                        </p>
+                     </div>
+                     <div className="block-right">
+                        <p>
+                           <strong>Density:</strong> {moon?.density} g/cm^3
+                        </p>
+                        <p>
+                           <strong>Gravity:</strong> {moon?.gravity} m/s^2
+                        </p>
+                        <p>
+                           <strong>Escape Velocity:</strong> {moon?.escape} m/s
+                        </p>
+                        <p>
+                           <strong>Mean Radius:</strong> {moon?.meanRadius} km
+                        </p>
+                        <p>
+                           <strong>Equatorial Radius:</strong> {moon?.equaRadius} km
+                        </p>
+                        <p>
+                           <strong>Polar Radius:</strong> {moon?.polarRadius} km
+                        </p>
+                        <p>
+                           <strong>Sidereal Orbit:</strong> {moon?.sideralOrbit} days
+                        </p>
+                        <p>
+                           <strong>Sidereal Rotation:</strong> {moon?.sideralRotation} hours
+                        </p>
+                     </div>
+                  </div>
                </>
             ) : (
                <p>...loading</p>
